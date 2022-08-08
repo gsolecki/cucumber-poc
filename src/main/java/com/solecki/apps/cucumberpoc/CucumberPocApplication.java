1bc84c3f631c1dab868eccb69d85156789203922
Grzegorz Solecki <gsolecki8@gmail.com>
2022-08-08 11:49:43 -0400

    Init commit.

---
+++ src/main/java/com/solecki/apps/cucumberpoc/CucumberPocApplication.java
@@ -0,0 +1,13 @@
+package com.solecki.apps.cucumberpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CucumberPocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CucumberPocApplication.class, args);
+    }
+
+}
